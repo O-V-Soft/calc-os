2abc743de77a465b27fc45f88cd8c0c3f79c61d5
CalcOS-Dev <ggcc98765432110@gmail.com>
2026-09-16 09:22:12 +0300

    CalcOS

--- Calc3D/calc3d.c
+++ Calc3D/calc3d.c
@@ -9,33 +9,43 @@
 #include <utils.h>
 #endif
 
+int is_face_visible(Point2D p0, Point2D p1, Point2D p2) {
+    return ((p1.x - p0.x) * (p2.y - p0.y) - (p1.y - p0.y) * (p2.x - p0.x)) > 0;
+}
+
 void draw_cube(int pos_x, int pos_y, int pos_z, int angle_y, int angle_x) {
     Point2D points2d[8];
 
     for (int i = 0; i < 8; i++) {
         Point3D node = cube_nodes[i];
-
         node = rotate_y(node, angle_y);
         node = rotate_x(node, angle_x);
-
         node.z += pos_z;
-
         points2d[i] = project(node, pos_x, pos_y);
     }
 
-    for (int i = 0; i < 12; i++) {
-        Point2D p1 = points2d[cube_edges[i].a];
-        Point2D p2 = points2d[cube_edges[i].b];
+    struct Face { 
+        int v[4]; 
+        uint8_t color; 
+    } faces[6] = {
+        {{0, 1, 2, 3}, COLOR_BLACK},   
+        {{4, 5, 1, 0}, COLOR_DARK_GRAY},    
+        {{4, 0, 3, 7}, COLOR_DARK_GRAY}, 
+        {{1, 5, 6, 2}, COLOR_DARK_GRAY}, 
+        {{3, 2, 6, 7}, COLOR_DARK_GRAY}, 
+        {{5, 4, 7, 6}, COLOR_DARK_GRAY}       
+    };
 
-        draw_line(p1.x, p1.y, p2.x, p2.y, COLOR_WHITE);
+    for (int i = 0; i < 6; i++) {
+        Point2D p0 = points2d[faces[i].v[0]];
+        Point2D p1 = points2d[faces[i].v[1]];
+        Point2D p2 = points2d[faces[i].v[2]];
+        Point2D p3 = points2d[faces[i].v[3]];
+
+        if (is_face_visible(p0, p1, p2)) {
+            draw_filled_triangle(p0, p1, p2, faces[i].color);
+            draw_filled_triangle(p0, p2, p3, faces[i].color);
+        }
     }
-
-    draw_filled_triangle(points2d[4], points2d[0], points2d[3], COLOR_DARK_GRAY);
-    draw_filled_triangle(points2d[4], points2d[3], points2d[7], COLOR_DARK_GRAY);
-
-    draw_filled_triangle(points2d[4], points2d[5], points2d[1], COLOR_DARK_GRAY);
-    draw_filled_triangle(points2d[4], points2d[1], points2d[0], COLOR_DARK_GRAY);
-
-    draw_filled_triangle(points2d[0], points2d[1], points2d[2], COLOR_BLACK);
-    draw_filled_triangle(points2d[0], points2d[2], points2d[3], COLOR_BLACK);
 }
+
